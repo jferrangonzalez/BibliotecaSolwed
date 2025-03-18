3912bf1be063f0fd6aa273b0ae1d99354378c865
jferrangonzalez <33357904+jferrangonzalez@users.noreply.github.com>
2025-03-18 08:03:21 +0100

    Biblioteca Solwed

---
+++ Init.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FacturaScripts\Plugins\BibliotecaSolwed;
+
+
+
+class Init extends \FacturaScripts\Core\Base\InitClass
+{
+    public function init()
+    {
+
+    }
+
+    public function update()
+    {
+    }
+}
